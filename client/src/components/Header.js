7716008a1bf8c9fff83012f3b52519248ac8e9c2
James-HPJ <85088804+James-HPJ@users.noreply.github.com>
2021-10-21 23:30:28 +0800

    Updated NavLinks in header.js
    -npm i react-router-bootstrap
    -wrapped all Navlinks with Linkcontainers, (all hrefs replaced with 'to' in Linkcontainers)
    -this will abide by the react 1 page rendering effect

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,21 @@
-
-import './Header.css';
+import './Header.css'
 import React, { useContext, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import { logOut } from '../utils/authenticate'
-import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
+import { Navbar, Nav, Container, Row, Col, NavLink } from 'react-bootstrap'
 import { UserContext } from '../App.js'
 import MyProfile from './MyProfile'
 import Logo from '../components/Logo'
 import { SearchBar } from './SearchBar'
 
 const Header = () => {
-  const userData = useContext(UserContext);
+  const userData = useContext(UserContext)
   const cartItems = useSelector((state) => state.cart.cartItems)
   //const history = useHistory();
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [numberItems, setNumberItems] = useState(0);
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [numberItems, setNumberItems] = useState(0)
 
   useEffect(() => {
     if (userData.userId !== '') {
@@ -23,13 +23,11 @@ const Header = () => {
     }
   }, [userData])
 
-  
-
   useEffect(() => {
-    const cartItemsStored = localStorage.getItem('cartItems');
+    const cartItemsStored = localStorage.getItem('cartItems')
     if (cartItemsStored) {
-      setNumberItems(JSON.parse(cartItemsStored).length);
-    } 
+      setNumberItems(JSON.parse(cartItemsStored).length)
+    }
   }, [cartItems])
 
   // console.log("decodedToken", decodedToken);
@@ -49,79 +47,94 @@ const Header = () => {
   return (
     <header id='header'>
       <Navbar bg='info' expand='sm' collapseOnSelect className='p-0'>
-        <Container>          
+        <Container>
           <Navbar.Toggle aria-controls='basic-navbar-nav' className='ms-auto' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav>
               <Nav.Link href='https://www.instagram.com/langzeitlerner/'>
-                Follow us<i className="fab fa-instagram fa-lg ms-1"></i>
+                Follow us<i className='fab fa-instagram fa-lg ms-1'></i>
               </Nav.Link>
             </Nav>
             <Nav className='ms-auto'>
               {loggedIn && (
                 <>
-                  <Nav.Link href='/shops' >
-                    Shops
-                  </Nav.Link>
-                  <Nav.Link href='/users' >
-                    Users
-                  </Nav.Link>
-                  
-                  <Nav.Link href='/messenger' >
-                    <i class="far fa-bell fa-sm ml-2">Notifications</i>
-                  </Nav.Link>
+                  <LinkContainer to='/shops'>
+                    <Nav.Link>Shops</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/users'>
+                    <Nav.Link>Users</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/messenger'>
+                    <Nav.Link>
+                      <i class='far fa-bell fa-sm ml-2'>Notifications</i>
+                    </Nav.Link>
+                  </LinkContainer>
                   <Nav>
-                  <MyProfile handleLogout={handleLogout} />                  
+                    <MyProfile handleLogout={handleLogout} />
                   </Nav>
                 </>
               )}
               {loggedIn === false && (
                 <>
-                  <Nav.Link href='/shops' >
-                    Shops
-                  </Nav.Link>
-                  <Nav.Link href='/signup' >
-                    <i className='fas fa-user-plus'>SignUp</i>
-                  </Nav.Link>
-                  <Nav.Link href='/login' >
-                    <i className='fas fa-user'>Login</i>
-                  </Nav.Link>
+                  <LinkContainer to='/shops'>
+                    <Nav.Link>Shops</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/signup'>
+                    <Nav.Link>
+                      <i className='fas fa-user-plus'>SignUp</i>
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/login'>
+                    <Nav.Link>
+                      <i className='fas fa-user'>Login</i>
+                    </Nav.Link>
+                  </LinkContainer>
                 </>
               )}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Navbar bg='info' className="p-0" fill>
+      <Navbar bg='info' className='p-0' fill>
         <Container>
-                   
           <Col xs={2} sm={2} md={2} lg={1} xl={1}>
-            <Link to='/'>              
-                <Logo />              
+            <Link to='/'>
+              <Logo />
             </Link>
           </Col>
-          <Col xs={9} sm={9} md={9} lg={8} xl={8} className="justify-content-center">
+          <Col
+            xs={9}
+            sm={9}
+            md={9}
+            lg={8}
+            xl={8}
+            className='justify-content-center'
+          >
             <SearchBar />
           </Col>
-          
-          <Col xs={2} sm={2} md={2} lg={1} xl={1}>          
+
+          <Col xs={2} sm={2} md={2} lg={1} xl={1}>
             <Nav>
-              <Nav.Link href={ loggedIn ? "/cart" : "/login"} >
-                <i className='fas fa-shopping-cart fa-xl'></i>
-                <span style={{
-                  position: 'relative',
-                  backgroundColor: '#ff7f7f',
-                  top: '-15px',
-                  left: '-10px',
-                  borderRadius: '8px',
-                  fontSize: '10px',
-                  padding: '5px'
-                }}>{numberItems}</span>                     
-              </Nav.Link>              
-            </Nav>       
-                
-          </Col>          
-                  
+              <LinkContainer to={loggedIn ? '/cart' : '/login'}>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart fa-xl'></i>
+                  <span
+                    style={{
+                      position: 'relative',
+                      backgroundColor: '#ff7f7f',
+                      top: '-15px',
+                      left: '-10px',
+                      borderRadius: '8px',
+                      fontSize: '10px',
+                      padding: '5px',
+                    }}
+                  >
+                    {numberItems}
+                  </span>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Col>
         </Container>
       </Navbar>
     </header>
